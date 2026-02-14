c2a03edf195f484efa58bdda3b88057e8865e6d0
DeftestHawk <deftesthawk@gmail.com>
2026-02-14 03:52:06 +0000

    Add files via upload

---
+++ assets/js/bootstrap.js
@@ -0,0 +1,14 @@
+(() => {
+  const BASE = "/grimshire-moddev";
+  const VERSION = "1";
+
+  const css = document.createElement("link");
+  css.rel = "stylesheet";
+  css.href = `${BASE}/assets/css/style.css?v=${VERSION}`;
+  document.head.appendChild(css);
+
+  const app = document.createElement("script");
+  app.src = `${BASE}/assets/js/app.js?v=${VERSION}`;
+  app.defer = true;
+  document.head.appendChild(app);
+})();
